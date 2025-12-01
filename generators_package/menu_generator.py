69f1fa686570359d99a68e20a9f1171abfe6170e
Adam <cyzksz59mf@privaterelay.appleid.com>
2025-12-01 18:01:03 +0000

    Added menu docstrings

--- generators_package/menu_generator.py
+++ generators_package/menu_generator.py
@@ -4,29 +4,45 @@
 
 from typing import Dict, Callable, Any
 
+
 class Menu:
     """Menu generator.
 
     ## Description
+    Base class for menus.
     ## Attributes
+    ```
+    self.name: str # Menu name
+    self.current_selection: int # Stores the index to the currently selected option
+    self.options: Dict[Callable[[], str], Callable[..., Any]] # Stores the options
+    ```
     ## Methods
+    ```
+    get_option(self, index: int) -> Callable[..., Any] # Return the option located at index `index`.
+    get_labels(self) -> list[str] # Return the current label text for each option.
+    ```
     """
 
-    def __init__(self, options, name='', current_selection=0) -> None:
+    def __init__(
+        self,
+        options: Dict[Callable[[], str], Callable[..., Any]],
+        name: str = "",
+        current_selection: int = 0,
+    ) -> None:
         """Initilise menu."""
-        self.name=name
-        self.current_selection=current_selection
+        self.name: str = name
+        self.current_selection: int = current_selection
         self.options: Dict[Callable[[], str], Callable[..., Any]] = options
 
-    def get_option(self, index: int):
+    def get_option(self, index: int) -> Callable[..., Any]:
         """Return the option located at index `index`."""
-        counter=0
+        counter = 0
         for option in list(self.options.keys()):
-            if counter==index:
+            if counter == index:
                 return self.options[option]
-            counter+=1
+            counter += 1
         raise NotImplementedError
 
-    def get_labels(self):
+    def get_labels(self) -> list[str]:
         """Return the current label text for each option."""
         return [label_func() for label_func in self.options.keys()]
