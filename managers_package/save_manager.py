76d236429ee65e8bf9f146b6c134968e2f8ceaf9
Adam <cyzksz59mf@privaterelay.appleid.com>
2025-12-02 15:22:16 +0000

    fixed saving

--- managers_package/save_manager.py
+++ managers_package/save_manager.py
@@ -3,19 +3,28 @@
 # -- Imports --
 
 import json
+import os
 
 
-def save_game(save_name, player_data, overworld_data, weapon_registry, item_registry, debugger):
+def save_game(
+    save_name, player_data, overworld_data, weapon_registry, item_registry, debugger
+):
     """Save the game state and player with a given savename in src/game_data."""
     compiled_data = {
         "player_data": player_data,
         "overworld_data": overworld_data,
-        'weapon_registry': weapon_registry,
-        'item_registry': item_registry
+        "weapon_registry": weapon_registry,
+        "item_registry": item_registry,
     }
-    with open(f"game_data/{save_name}.json", "w") as file:
-        json.dump(compiled_data, file, indent=4)
-    debugger.write(f'Game saved to game_data/{save_name}')
+    while True:
+        try:
+            with open(f"game_data/{save_name}.json", "w") as file:
+                json.dump(compiled_data, file, indent=4)
+            break
+        except:
+            os.mkdir('game_data')
+
+        debugger.write(f"Game saved to game_data/{save_name}")
     quit()
 
 
@@ -23,5 +32,5 @@ def load_game(save_name, debugger):
     """Load a game save from src/game_data."""
     with open(f"game_data/{save_name}.json", "r") as file:
         saved_data = json.load(file)
-    debugger.write('Loaded game data')
+    debugger.write("Loaded game data")
     return saved_data
